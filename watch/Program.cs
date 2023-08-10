a2050a0507f4e833a236ab86e940472406a47ad2
Guy Langston <guylangston@gmail.com>
2023-08-10 20:22:44 +0100

    feat: regex match; fix: file vs dir match

--- watch/Program.cs
+++ watch/Program.cs
@@ -1,14 +1,52 @@
-var file = args.First();
-var full = new FileInfo(file);
-var dir = full.Directory.FullName;
-using var watcher = new FileSystemWatcher(dir);
+// args[0] = directory/file
+// args[1, optional = regex match (ignored if args[0] is file)
+
+using System.Text.RegularExpressions;
+
+var watchPath = args.First();
+if (!Path.Exists(watchPath)) throw new Exception($"Path not found: '{watchPath}'");
+
+Regex? match = null;
+if (args.Length >= 2)
+{
+    match = new Regex(args[1]);
+}
+
+
+var path = GetPathInfo(watchPath);
+var dir = path is DirectoryInfo d ? d : ((FileInfo)path).Directory!;
+
+using var watcher = new FileSystemWatcher(dir.FullName);
 
 watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite;
 watcher.Changed += (o,e) => {
-    if (full.FullName == e.FullPath) {
+    if (IsMatch(e))
+    {
         Console.WriteLine(e.FullPath);
     }
 };
+
+
 watcher.EnableRaisingEvents = true;
 
 Console.ReadLine();
+
+
+static FileSystemInfo GetPathInfo(string path)
+{
+    if (Directory.Exists(path)) return new DirectoryInfo(path);
+    if (File.Exists(path)) return new FileInfo(path);
+    throw new Exception($"Path not found: {path}");
+}
+bool IsMatch(FileSystemEventArgs e)
+{
+    if (match != null)
+    {
+        return match.IsMatch(e.FullPath);
+    }
+    if (path is FileInfo fileInfo)
+    {
+        return fileInfo.FullName == e.FullPath;
+    }
+    return false;
+}
