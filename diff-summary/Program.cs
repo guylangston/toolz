ad3d3ca41c78598683fd5661cdf660fe9d8c5aa0
Guy Langston <guy.langston@fenics.com>
2023-08-14 16:12:58 +0100

    feat: -1 switch will show just the largest diff chunk per file

--- diff-summary/Program.cs
+++ diff-summary/Program.cs
@@ -1,4 +1,3 @@
-using System;
 //Sample
 // diff --git a/src/Website.BackEnd/SignalR/Hubs/RfsHubImpl.cs b/src/Website.BackEnd/SignalR/Hubs/RfsHubImpl.cs
 // @@ -35,8 +35,6 @@ using Microsoft.AspNetCore.SignalR;
@@ -6,10 +5,14 @@ using System;
 // @@ -28,7 +28,6 @@ using System;
 // @@ -60,9 +59,12 @@ public class CancellableItemCache<TKey, TValue>
 // @@ -187,4 +189,4 @@ public class CancellableItemCache<TKey, TValue>
+//
+
+var argOne = args.Length > 0 && args.Any(x=>x == "-1");
 
 var lastDiff = "";
 var file = "";
 var ln = 0;
+var fileDiffs = new List<(string txt, int size)>();
 while( Console.ReadLine() is {} line) 
 {
     ln++;
@@ -17,6 +20,7 @@ while( Console.ReadLine() is {} line)
     {
         if (line.StartsWith("diff "))
         {
+            PushFileDiffs();
             lastDiff = line;
             var parts = line.Split(' ');
             file = parts[2].Remove(0, 2);
@@ -28,14 +32,34 @@ while( Console.ReadLine() is {} line)
             var psp = prefix.Trim().Split(' ');
             var ppsp = psp[1].Split(',');
             var cln = int.Parse(ppsp[0]);
+            var size = int.Parse(ppsp[1]);
 
             var summary = line[(split+2)..];
-            Console.WriteLine($"{file}|{cln}| {psp[1]} => {summary}");
+            fileDiffs.Add(($"{file}|{cln}| {psp[1]} => {summary}", size));
         }
     }
     catch(Exception ex)
     {
         Console.Error.WriteLine($"Line: {ln} | {line}");
         Console.Error.WriteLine(ex.ToString());
+    }
+}
+PushFileDiffs();
+
+void PushFileDiffs()
+{
+    if (fileDiffs.Any()) 
+    {
+        if (argOne)
+        {
+            Console.WriteLine(fileDiffs.MaxBy(x=>x.size).txt);
+        }
+        else {
+            foreach(var line in fileDiffs)
+            {
+                Console.WriteLine(line.txt);
+            }
+        }
+        fileDiffs.Clear();
     }
 }
