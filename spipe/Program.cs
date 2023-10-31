9f33441e966b32526975975b8e10210721ffc4fd
Guy Langston <guy.langston@fenics.com>
2023-10-31 08:50:12 +0000

    spipe

---
+++ spipe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length > 1 && args[0] == "cols")
+        {
+            var split = args[1];
+            var all = new List<string>();
+            while (Console.ReadLine() is { } ln)
+            {
+                all.Add(ln);
+            }
+            var max = all.Max(x => x.IndexOf(split));
+            if (max >= 0)
+            {
+                foreach (var ln in all)
+                {
+                    var idx = ln.IndexOf(split);
+                    if (idx < 0)
+                    {
+                        Console.WriteLine(ln);
+                    }
+                    else
+                    {
+                        Console.Write(ln[0..idx].PadRight(max));
+                        Console.Write(split);
+                        Console.WriteLine(ln[(idx + split.Length)..]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ spipe/build.ps1
@@ -0,0 +1,8 @@
+
+if ( (Test-Path ../bflat)) {
+  echo "[bflat] using bflat build"
+  ..\bflat\bflat.exe build .\Program.cs --os:windows -o ../spipe.exe
+  exit 0;
+}
+dotnet publish -c Release --sc -r win-x64 -p:PublishTrimmed=true -p:PublishSingleFile=true -o ../
+
