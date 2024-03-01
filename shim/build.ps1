a7a0847b7806f46170fb607e5332a9ad6428fbf2
Guy Langston <guylangston@hotmail.com>
2024-03-01 14:58:58 +0000

    fixed: shim name

--- shim/build.ps1
+++ shim/build.ps1
@@ -1,8 +1,13 @@
-$outName = "vi.exe"
+$outName = "shim.exe"
 if ( (Test-Path ../bflat)) {
   echo "[bflat] using bflat build"
   ..\bflat\bflat.exe build .\Program.cs --os:windows -o ../$outName
   exit 0;
 }
+else {
+  echo "[bflat] missing. see: https://github.com/bflattened/bflat/releases"
+  echo "[bflat] skipping, using 'dotnet publish'"
+}
+
 dotnet publish -c Release --sc -r win-x64 -p:PublishTrimmed=true -p:PublishSingleFile=true -o ../
 
