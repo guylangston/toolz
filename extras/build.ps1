2933d4a8ed7ab56592f56840731dec05a2eb7f70
Guy Langston <guylangston@hotmail.com>
2024-03-13 09:07:48 +0000

    quite download

--- extras/build.ps1
+++ extras/build.ps1
@@ -1,4 +1,5 @@
 if ((Test-Path ./strip-ansi-x86_64-pc-windows-gnu.exe) -eq $false) {
-    wget https://github.com/KSXGitHub/strip-ansi-cli/releases/download/0.1.0/strip-ansi-x86_64-pc-windows-gnu.exe
+    echo "[download] strip-ansi-cli"
+    wget -q https://github.com/KSXGitHub/strip-ansi-cli/releases/download/0.1.0/strip-ansi-x86_64-pc-windows-gnu.exe
 }
 cp ./strip-ansi-x86_64-pc-windows-gnu.exe ../strip-ansi.exe
