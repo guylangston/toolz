326125a20257e9488bce39d9481f61bab5d844be
Guy Langston <guy.langston@fenics.com>
2024-05-02 09:56:12 +0100

    feat: humanise time

--- time/Program.cs
+++ time/Program.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-internal class Program
+internal static class Program
 {
     private static int Main(string[] args)
     {
@@ -13,8 +13,20 @@ internal class Program
         timer.Start();
         var ret = ProcessRunInlineConsole(args.First(), args.Skip(1).ToArray());
         timer.Stop();
-        Console.WriteLine($"Completed: {timer.Elapsed}");
+        Console.WriteLine($"Completed: {timer.Elapsed} -- {Humanize(timer.Elapsed)}");
         return ret;
+    }
+
+    public static string Humanize(TimeSpan span)
+    {
+        if (span.TotalSeconds < 1) return $"{span.Milliseconds} ms";
+        if (span.TotalMinutes < 1) return $"{span.Seconds} sec, {span.Milliseconds} ms";
+        if (span.TotalHours < 1)   return $"{span.Minutes} min, {span.Seconds} sec";
+        if (span.TotalDays < 1)    return $"{span.Hours} hr, {span.Minutes} min";
+        if (span.TotalDays > 365)  return $"{(int)span.TotalDays / 365} yrs, {(int)span.TotalDays % 365} days";
+
+        if (span.Hours == 0) return $"{span.Days} days";
+        return $"{span.Days} days, {span.Hours} hr";
     }
 
     public static int ProcessRunInlineConsole(string execPath, string[] args)
